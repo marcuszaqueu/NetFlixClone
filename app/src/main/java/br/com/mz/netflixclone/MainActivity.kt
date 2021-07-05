132b9506ff0163c7f2dd887f9f847a85b2cf2658
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-07-05 10:11:08 -0300

    App CLone Netflix Criação da tela inical para carregamento Criação da tela de acesso com login e senha

--- app/src/main/java/br/com/mz/netflixclone/MainActivity.kt
+++ app/src/main/java/br/com/mz/netflixclone/MainActivity.kt
@@ -1,11 +1,27 @@
 package br.com.mz.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            abrirTelaLogin()
+        },3500)
+    }
+
+
+    private fun abrirTelaLogin(){
+        val intent = Intent(this,frmLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
