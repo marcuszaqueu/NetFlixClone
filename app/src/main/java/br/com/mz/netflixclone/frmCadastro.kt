594ed31f896c64e8b37a8ad93b5c33e637692a50
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-07-05 11:02:01 -0300

    Iniciando configuracao da tela de cadastro para acesso ao firebase

--- app/src/main/java/br/com/mz/netflixclone/frmCadastro.kt
+++ app/src/main/java/br/com/mz/netflixclone/frmCadastro.kt
@@ -1,5 +1,6 @@
 package br.com.mz.netflixclone
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,17 +10,32 @@ class frmCadastro : AppCompatActivity() {
 
     private lateinit var binding: ActivityFrmCadastroBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFrmCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+        Toolbar()
 
+        binding.btnCadastrar.setOnClickListener {
+
+            val email = binding.cadEmail.text.toString()
+            val senha = binding.cadSenha.text.toString()
+            val mensagem_erro = binding.mensagemErro
+
+            if(email.isEmpty() || senha.isEmpty()){
+                mensagem_erro.setText("Preencha todos os campos")
+
+            }
+        }
+
+    }
+
+    private fun Toolbar(){
         val toolbar = binding.toolbarCadastro
         toolbar.setBackgroundColor(getColor(R.color.white))
         toolbar.setNavigationIcon(getDrawable(R.drawable.ic_netflix_official_logo))
-
-
     }
 }
