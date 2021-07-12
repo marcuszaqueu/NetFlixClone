50c607c3e43d95f223e8b7df6b1081709c70fa75
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-07-12 12:55:37 -0300

    Autenticando Usuário

--- app/src/main/java/br/com/mz/netflixclone/frmCadastro.kt
+++ app/src/main/java/br/com/mz/netflixclone/frmCadastro.kt
@@ -2,8 +2,14 @@ package br.com.mz.netflixclone
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.mz.netflixclone.databinding.ActivityFrmCadastroBinding
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthEmailException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class frmCadastro : AppCompatActivity() {
 
@@ -26,11 +32,42 @@ class frmCadastro : AppCompatActivity() {
 
             if(email.isEmpty() || senha.isEmpty()){
                 mensagem_erro.setText("Preencha todos os campos")
-
+            }else{
+                mensagem_erro.setText("")
+                CadastrarUsuario()
             }
         }
 
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun CadastrarUsuario(){
+        val email = binding.cadEmail.text.toString()
+        val senha = binding.cadSenha.text.toString()
+        val mensagem_erro = binding.mensagemErro
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this,"Usuário cadastrado com sucesso!",Toast.LENGTH_SHORT).show()
+                binding.cadEmail.setText("")
+                binding.cadSenha.setText("")
+                mensagem_erro.setText("")
+            }
+        }.addOnFailureListener {
+            var erro = it
+
+            when{
+                erro is FirebaseAuthWeakPasswordException -> mensagem_erro.setText("A senha deve possuir no minimo \n6 carecteres!")
+                erro is FirebaseAuthUserCollisionException -> mensagem_erro.setText("Este em-mail já existe!")
+                erro is FirebaseAuthEmailException -> mensagem_erro.setText("Digite um e-mail válido!")
+                erro is FirebaseNetworkException -> mensagem_erro.setText("Verifique sua conexão de internet!")
+                else -> mensagem_erro.setText("Erro ao cadastrar usuário!")
+            }
+
+
+        }
+    }
+
 
     @SuppressLint("UseCompatLoadingForDrawables")
     private fun Toolbar(){
--- app/src/main/java/br/com/mz/netflixclone/frmLogin.kt
+++ app/src/main/java/br/com/mz/netflixclone/frmLogin.kt
@@ -3,7 +3,9 @@ package br.com.mz.netflixclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.mz.netflixclone.databinding.ActivityFrmLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class frmLogin : AppCompatActivity() {
 
@@ -21,5 +23,41 @@ class frmLogin : AppCompatActivity() {
             val intent = Intent(this,frmCadastro::class.java)
             startActivity(intent)
         }
+
+        binding.btEntrar.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagem_erro = binding.mErroLogin
+
+            if(email.isEmpty() || senha.isEmpty()){
+                mensagem_erro.setText("Preencha todos os Campos!")
+            }else{
+                AutenticarUsuario()
+
+            }
+        }
     }
+
+    private fun AutenticarUsuario(){
+        val email = binding.editEmail.text.toString()
+        val senha = binding.editSenha.text.toString()
+        val mensagem_erro = binding.mErroLogin
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this,"Login efetuado com sucesso!",Toast.LENGTH_SHORT).show()
+                LoginTelaFilmes()
+            }
+        }.addOnFailureListener {
+            mensagem_erro.setText("Erro ao logar!")
+        }
+    }
+
+    private fun LoginTelaFilmes(){
+        val intent = Intent(this,ListaFilmes::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
